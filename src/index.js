5c6ab7ee3e63e41a63be646561ab58c9f1fc4e41
lg0leg <79754202+lg0leg@users.noreply.github.com>
2021-03-15 23:02:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length < 1) {return (0)}
+  else
+    {
+      var mi = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (mi > array[i]) mi = array[i]};
+      return mi
+    }
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length < 1) {return (0)}
+  else
+    {
+      var ma = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (ma < array[i]) ma = array[i]};
+      return ma
+    }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length < 1) {return (0)} 
+  else
+    {
+      var av = 0;
+      for (var i = 0; i < array.length; i++) {
+      av = av + array[i]};
+    return(av/array.length)
+    }
+
 }
